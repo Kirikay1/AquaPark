00e1f153f6fe9eb67b633b57f00111cfe48fc950
Maxim <maksim.timoshin.045@gmail.com>
2026-05-24 18:52:49 +0300

    feat: Add menu navigation with MVVM command

---
+++ AquaPark/ViewModel/MenuViewModel.cs
@@ -0,0 +1,25 @@
+﻿using AquaPark.Services;
+using AquaPark.Views;
+using System.Windows;
+using System.Windows.Input;
+
+namespace AquaPark.ViewModel
+{
+    public class MenuViewModel : BaseViewModel
+    {
+        public ICommand ClientsCommand { get; }
+
+        public MenuViewModel()
+        {
+            ClientsCommand = new RelayCommand(OpenClientsPage);
+        }
+
+        private void OpenClientsPage(object? parameter)
+        {
+            if (Application.Current.MainWindow is MainWindow mainWindow)
+            {
+                mainWindow.OpenPage(new ClientsPage());
+            }
+        }
+    }
+}
---
+++ AquaPark/Views/ClientsPage.xaml.cs
@@ -0,0 +1,22 @@
+﻿using AquaPark.Data;
+using System.Windows.Controls;
+
+namespace AquaPark.Views
+{
+    /// <summary>
+    /// Логика взаимодействия для ClientsPage.xaml
+    /// </summary>
+    public partial class ClientsPage : Page
+    {
+        public ClientsPage()
+        {
+            InitializeComponent();
+            LoadClients();
+        }
+
+        private void LoadClients()
+        {
+            ClientsDataGrid.ItemsSource = AppData.db.Clients.ToList();
+        }
+    }
+}
